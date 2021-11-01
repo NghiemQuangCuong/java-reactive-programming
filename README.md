9ba775607f1d4431eef8057d9249dcf349286c6a
CuongNghiem <quangcuongnghiem@gmail.com>
2021-11-01 18:51:51 +0700

    Stream parallel, Map vs FlatMap

---
+++ README.md
@@ -0,0 +1,3 @@
+Understand java stream:
+https://medium.com/swlh/understanding-java-streams-e0f2df12441f
+
--- src/main/java/com/example/javaplayground/bootstrap/Bootstrap.java
+++ src/main/java/com/example/javaplayground/bootstrap/Bootstrap.java
@@ -3,6 +3,7 @@ package com.example.javaplayground.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,16 +18,40 @@ public class Bootstrap implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // streamCreation();
+        // streamParallel();
+        // streamMap();
+        // streamFlatMap()
 
-        Integer[] integers = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        Stream<Integer> stream = Arrays.stream(integers);
-        stream.parallel().forEach(System.out::println);
+
     }
 
     private void streamCreation(){
         String[] arr = new String[]{"a", "b", "c", "a"};
         Stream<String> stream = Arrays.stream(arr);
         List<String> list = stream.filter(c -> c.equals("a")).collect(Collectors.toList());
+        list.forEach(System.out::println);
+    }
+
+    private void streamParallel(){
+        Integer[] integers = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        Stream<Integer> stream = Arrays.stream(integers);
+        stream.parallel().forEach(System.out::println);
+    }
+
+    public void streamMap(){
+        String[] fruits = new String[]{"Apple", "Lemon", "Banana", "Dragon Fruit"};
+        Stream<Integer> stream = Arrays.stream(fruits).map(String::length);
+        stream.forEach(System.out::println);
+    }
+
+    public void streamFlatMap(){
+        List<List<Integer>> numbers = new ArrayList<>();
+        numbers.add(Arrays.asList(1, 2, 3));
+        numbers.add(Arrays.asList(4));
+        numbers.add(Arrays.asList(5, 6, 7, 8));
+        numbers.add(Arrays.asList(9, 10, 11));
+        numbers.add(Arrays.asList(12, 13));
+        List<Integer> list = numbers.stream().flatMap(a -> a.stream()).collect(Collectors.toList());
         list.forEach(System.out::println);
     }
 }
